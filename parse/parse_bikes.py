62e3a80adde00e9fcb1581d5c56dc4c45801c585
Philip Shemella <philip@skunksports.com>
2020-01-25 23:07:01 +0100

    cleanup parsing script

--- parse/parse_bikes.py
+++ parse/parse_bikes.py
@@ -9,18 +9,27 @@ import pandas as pd
 
 def main():
 
+	frames = []
+
+	# find all matching files
 	files = glob.glob('..'+os.sep+'data'+os.sep+'*.geojson')
 
-	frames = []
 	for f in files:
 		with open(f,'r') as fo:
 			data = json.load(fo)
+
+		# load json into dataframe	
 		df = pd.io.json.json_normalize(data.get('features'))
+		
+		# split array of geocoords into two columns
 		df['longitude'], df['latitude'] = zip(*df['geometry.coordinates'])
+		
+		# cleanup timestamps
 		df['timestamp'] = df['properties.time'].combine_first(df['properties.timeStamp'])
 		# "01/25/2020, 21:51:45"
 		df['timestamp'] = pd.to_datetime(df.timestamp, format='%m/%d/%Y, %H:%M:%S', errors='ignore')
 
+		# drop columns
 		del df['properties.time']
 		del df['properties.timeStamp']
 		del df['geometry.coordinates']
@@ -29,25 +38,36 @@ def main():
 		del df['properties.zip']
 		del df['type']
 
-		df.rename(columns={'properties.range': 'range', \
-							'properties.charge': 'charge', \
-							'properties.manual': 'manual', \
-							'properties.electric': 'electric', \
-							'properties.id': 'id', \
-							'properties.name': 'name', \
-							'properties.provider': 'provider', \
-							'properties.size': 'size'}, \
-							inplace=True)
-
+		# add filename to dataframe
 		df['filename'] = f.split(os.sep)[-1]
+		
 		#print(df.columns)
 		#df['timestamp'].to_csv('test.csv')
 		#break
 
+		# add this file's frame to the list of frames
 		frames.append(df)
 
+	# combine all dataframes into one
 	df = pd.concat(frames)
-	df.to_csv('bikes.csv',sep='\t')
+
+	# rename columns
+	df.rename(columns={'properties.range': 'range', \
+						'properties.charge': 'charge', \
+						'properties.manual': 'manual', \
+						'properties.electric': 'electric', \
+						'properties.id': 'id', \
+						'properties.name': 'name', \
+						'properties.provider': 'provider', \
+						'properties.size': 'size'}, \
+						inplace=True)
+
+	# rearrange columns to make more sense
+	#print(df.columns)
+	df = df[['filename','provider', 'id', 'timestamp', 'longitude', 'latitude', \
+				'name', 'charge', 'electric', 'manual',  'range', 'size']]
+
+	df.to_csv('bikes.csv',sep=',',index=False)
 
 if __name__ == "__main__":
 	main()
